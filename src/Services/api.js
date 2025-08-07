45eca667fdb13fd0228584b9250f583895db1ee5
carlos.iglesias <carlos.iglesias@linktic.com>
2025-08-07 03:27:07 -0500

    Frontend ini

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8083/api',
+  baseURL: 'https://carrito-compras-tecnica-2025-production.up.railway.app/api',
 });
 
 
